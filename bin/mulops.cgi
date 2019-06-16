62bb3208aa1923a639cbdd8ecd0366fbba8a15e4
Richard Jamieson <richard.jamieson@sscsuk.net>
2019-06-16 16:44:54 +0000

    initial commit

---
+++ bin/mulops.cgi
@@ -0,0 +1,338 @@
+#!/usr/bin/perl
+# Richard Jamieson : 15/6/2016
+# Based on functional spec (20160428-1)
+# See: https://sourceforge.net/projects/mulops
+# Catalogs in JSON format
+#
+#RJuse strict;
+use warnings;
+use Getopt::Std;
+use File::Basename;
+use File::Spec;
+
+###########################################################################################
+# Set a default location for catalogs and perl modules
+my $default_catalogs_dir ;
+my $default_models_dir ;
+
+BEGIN
+   {
+      my $default_perlmods_dir ;
+      my $default_doc_dir ;
+      my $mulops_bin = dirname($0) ;
+      my $abs_path_mulops_bin = File::Spec->rel2abs( $mulops_bin ) ;
+      my $mulops_top_dir = dirname($abs_path_mulops_bin) ;
+      $ENV{PATH} .= ":$abs_path_mulops_bin" ;
+      $default_catalogs_dir = "$mulops_top_dir/catalogs" ;
+      $default_models_dir = "$mulops_top_dir/models" ;
+      $default_doc_dir = "$mulops_top_dir/docs" ;
+      $ENV{MULOPS_DOCS} = $default_doc_dir ;
+      $default_perlmods_dir = dirname($abs_path_mulops_bin) . "/lib" ;
+      if ( -d $default_perlmods_dir ) {
+         push ( @INC,"$default_perlmods_dir");
+         $ENV{PERL5LIB} .= ":$default_perlmods_dir";
+      }
+   }
+use JSON;	# Only required when using "MULMODEL" variable
+use MULOPS;
+
+###########################################################################################
+# Process command line arguments ( See "mulops usage" )
+
+use CGI qw/:standard :html3/; 
+use CGI::Carp qw( fatalsToBrowser );
+
+my $JSCRIPT=<<EOF;
+<script language="JavaScript">
+function toggle(source) {
+  checkboxes = document.getElementsByName('mulops_picks');
+  for(var i=0, n=checkboxes.length;i<n;i++) {
+    checkboxes[i].checked = source.checked;
+  }
+}
+</script>
+EOF
+
+;
+
+print header;
+
+print start_html(-title => "mulops", -BGCOLOR=>'#6698FF');
+print "$JSCRIPT";
+print_prompt();
+print end_html;
+
+sub print_prompt {
+
+   print start_form;
+
+   # Get model
+   my $env_model = param('model');
+   $env_model = 'solaris11' unless defined $env_model ;
+
+   # Set mulops_mode based on model
+   my $mulops_mode ;
+   if ( $env_model eq 'Live' ) {
+      $mulops_mode = "execute";
+   } else {
+      $mulops_mode = "oscommand";
+   }
+
+   # Get mulops_picks (ie:what items to display )
+   my @mulops_picks = param('mulops_picks');
+   my $num_mulops_picks = scalar @mulops_picks ;
+   my $param_mulops_command_args;
+   if ( $num_mulops_picks eq 0 ) {
+      @mulops_picks = qw/all/ ;
+      $param_mulops_command_args = "all"
+   } else {
+      $param_mulops_command_args = "@mulops_picks" ;
+   }
+   my @mulops_command_args = split(' ', $param_mulops_command_args);
+
+   # Get json parameter ( determines if we just show JSON for one item )
+   my $param_json = param('json');
+
+   ###########################################################################################
+   # Get details of all catalogs - and create catalogs cache
+   my $env_catalogs = $ENV{'MULOPS_CATALOGS'};
+   my @catalogs = split(' ', $env_catalogs) if defined($env_catalogs);
+   if (! @catalogs) {
+      @catalogs = glob("$default_catalogs_dir/mulcat.*.json");
+   }
+   my %catalog_cache = map { $_ => {} } @catalogs;
+   my $catalog_cache = \%catalog_cache ;
+
+   ###########################################################################################
+   # MODELING CODE
+   # If a model is used, locad up the cache with the data from that model
+   my $mulops_output_cache  ;
+   my $model_link;
+   my $model_cache = {} ;
+   if ($env_model ne "Live" )  {
+      $model_link = "&model=$env_model";
+      my $model_file = "$default_models_dir/model.$env_model.json";
+      if ( ! -r $model_file ) { 
+         print "Cant find model file : $model_file\n" ;
+         exit 1 ;
+      } else {
+         local $/; #Enable 'slurp' mode
+         open my $fh, "<", "$model_file" or die "Cant open model file : $model_file";;
+         my $model_file_data = <$fh>;
+         close $fh;
+         $mulops_output_cache = from_json($model_file_data);
+         $model_cache = $mulops_output_cache ;
+      }
+   }
+   ###########################################################################################
+
+
+   ###########################################################################################
+   # Run mulops !
+
+   foreach my $mulops_command_arg (@mulops_picks) {
+      mulops($mulops_command_arg,$mulops_mode,$catalog_cache,$mulops_output_cache) ;
+   }
+
+   my $mulops_group = getSet("$default_catalogs_dir/mulgroup.default.json") ;
+   my @mulops_group_keys = keys $mulops_group->{'mulgroups'} ;
+   my $picked_group = param('groups');
+   my $group_items = $mulops_group->{'mulgroups'}->{$picked_group}->{group};
+
+   my %group_labels ;
+   %group_labels->{'all'} = 'All Categories';
+   foreach my $mulops_group_key (@mulops_group_keys) {
+      my $group_description = $mulops_group->{'mulgroups'}->{$mulops_group_key}->{description};
+      %group_labels->{$mulops_group_key} = $group_description ;
+   }
+
+   my @mulops_group_names = ('all');
+   push (@mulops_group_names, @mulops_group_keys);
+
+   my $param_filter = param('filter');
+   $param_filter = '' unless defined($param_filter) ;
+
+   my @vals_to_display;
+   if ( $param_mulops_command_args eq "all") {
+      @vals_to_display = sort keys %$mulops_output_cache ;
+      @vals_to_display = grep !/^all$/, @vals_to_display; # not sure how 'all' gets in there !
+      $mulops_output_cache = $model_cache ;
+   } else {
+      @vals_to_display = @mulops_picks;
+   }
+
+   ###########################################################################################
+   # Printing
+   if ( defined($param_json) ) {
+      print '<PRE>';
+      print "$mulops_output_cache->{$param_mulops_command_args}->{catalog}" ;
+      print '</PRE>';
+      exit 0;
+   }
+
+   print "<p>";
+   print '<a href=https://sourceforge.net/projects/mulops/><button type="button">Get Mulops</button></a>';
+   print '<a href=/catalogs><button type="button">Catalogs</button></a>';
+   print '<a href=/docs><button type="button">Docs</button></a>';
+   
+   print popup_menu(-name=>'model',
+                  -values=>[qw/windows10 ubuntu14 centos6 centos7 solaris10 solaris11 aix hpux/],
+                  -Onchange=>"this.form.submit()",
+                  -default=>'solaris11');
+
+   print popup_menu(-name=>'groups',
+                  -labels=>\%group_labels,
+                  -values=>[@mulops_group_names],
+                  -Onchange=>"this.form.submit()");
+
+   print '<INPUT TYPE="text" name="filter" onChange="this.form.submit()">';
+
+   print "Filtered on: $param_filter";
+   print "<br>";
+
+   print '<table border="1" width="100%">';
+
+   if ( $env_model ne 'Live' ) { 
+      print '<col width="2%">';
+      print '<col width="15%">';
+      print '<col width="25%">';
+      print '<col width="58%">';
+   } 
+      print '<tr>';
+         #print '<th><input type="checkbox" onClick="toggle(this)" checked></th>';
+         #RJprint '<th><input type="checkbox" onClick="toggle(this)" ></th>';
+         print '<th>';
+            print submit('Action','Select');
+         print '</th>';
+         print '<th>mulops-command</th>';
+         print '<th>description</th>';
+         if ( $env_model eq 'Live' ) { 
+            print '<th>output</th>';
+         }
+         print '<th>oscommand</th>';
+      print '<tr>';
+      foreach my $cache_value (@vals_to_display) {
+         my $display_row = 'n';
+         my $cat_name = $mulops_output_cache->{$cache_value}->{'cat_name'};
+         my $mulops_desc = $mulops_output_cache->{$cache_value}->{'description'} ;
+         my $mulops_out = $mulops_output_cache->{$cache_value}->{'execute'};
+         my $oscommand = $mulops_output_cache->{$cache_value}->{'oscommand'} ;
+         my $mulops_catalog_entry = $mulops_output_cache->{$cache_value}->{catalog} ;
+         if ( $oscommand =~ /^NoValues/ ) {
+            $oscommand="<a href=\"mailto:Richard.Jamieson\@scsuk.net?Subject=mulops $env_model $cache_value NoValues\" target=\"_top\">$oscommand</a>";
+         }
+         if ( (grep /^$cache_value$/, @$group_items) || (! defined($picked_group)) || ($picked_group eq 'all')   )  {
+            $display_row = 'y';
+         }
+         if ( $param_filter ne '') {
+            if ( ($cache_value =~ /$param_filter/i) || ($mulops_desc =~ /$param_filter/i) || ($oscommand =~ /$param_filter/i) )  {
+               $display_row = 'y';
+            } else {
+               $display_row = 'n';
+            }
+         }
+
+         if ( $display_row eq 'y' ) {
+         print '<tr>';
+            #print '<td><input type=CHECKBOX name=mulops_picks value="' . $cache_value . '" checked></td>';
+            print '<td align="center"><input type=CHECKBOX name=mulops_picks value="' . $cache_value . '" ></td>';
+            print '<td>';
+               print "<a href=\"/cgi-bin/mulops/bin/mulops.cgi?mulops_picks=$cache_value&json\">$cache_value</a>";
+            print '</td>';
+            print '<td>';
+               print "$mulops_desc";
+            print '</td>';
+            if ( $env_model eq 'Live' ) { 
+               print '<td>';
+                  print '<PRE>';
+                  print "$mulops_out";
+                  print '</PRE>';
+               print '</td>';
+            }
+            print '<td>';
+               if ( $cache_value =~ /.doc$/ ) {
+                  $mulops_doc = printDoc($oscommand) ;
+                  my $nr_of_lines = $mulops_doc =~ tr/\n//;
+                  if ( $nr_of_lines <= 10 ) {
+                     print '<PRE>';
+                     print "$mulops_doc";
+                     print '</PRE>';
+                  } else {
+                     print "<a href=\"/docs/$oscommand\">$oscommand</a>";
+                  }
+               } else {
+                  print "$oscommand";
+            print '</td>';
+            }
+         print '</tr>';
+         }
+   }
+
+   print '</table>';
+   print end_form;
+}
+
+
+
+###########################################################################################
+sub usage {
+   system("mulops usage");
+   exit;
+}
+###########################################################################################
+
+# Params = catalog
+# Return = hashref
+sub getSet {
+   # Can add another parameter later - to signify format and maybe source of catalog
+   my ($group) = @_;
+   #print "Get Set : $group \n";
+   my $mulops_group;
+   if ( $group =~ "^http:" ) {
+      my $http = HTTP::Lite->new;
+      my $req = $http->request("$group") or die "Unable to get document: $!";
+      die "Request failed ($req): ".$http->status_message() if $req ne "200";
+      my $mulops_url_data = $http->body();
+      $mulops_group = from_json($mulops_url_data);
+   } else {
+     my $mulops_file_data;
+     local $/; #Enable 'slurp' mode
+     open my $fh, "<", "$group";
+     $mulops_file_data = <$fh>;
+     close $fh;
+     $mulops_group = from_json($mulops_file_data);
+   }
+   return $mulops_group ;
+}
+
+
+###########################################################################################
+# POD info
+
+=head1 NAME
+
+ mulops - Multi Operating System tool
+          Simple consistent commands..
+
+=head1 SYNOPSIS
+
+ See "mulops manual" or "mulops usage"
+
+=head1 DESCRIPTION
+
+ Different operating systems have different ways of doing the same thing.
+ ( eg: showing total amount of system memory ).
+ This tool aims to help to provide a consistent way to do the same things
+ across multiple operating systems.
+ The tool can be used for one-off commands, to create portable system
+ programs, or just for education
+
+
+=head1 INSTALL
+
+ See "mulops readme"
+
+=head1 AUTHORS
+
+ Richard Jamieson richard.jamieson@scsuk.net
+
---
+++ lib/MULOPS.pm
@@ -0,0 +1,255 @@
+package MULOPS;
+# Richard Jamieson : 16/6/2016
+# Based on functional spec (20160428-1)
+# See: https://sourceforge.net/projects/mulops
+# Catalogs in JSON format
+# CATALOG: Valid operators :  Numeric(==,!=,<,>,<=,>=) String(eq,ne,lt,gt,le,ge) Match(=~ and value can be a RE)
+
+use strict;
+use base qw(Exporter);
+@MULOPS::EXPORT = qw(mulops printDoc);
+use warnings;
+use JSON;
+use HTTP::Lite;
+use Capture::Tiny::Extended qw/capture tee capture_merged tee_merged/;
+
+
+BEGIN {
+    $MULOPS::VERSION = '4.00';
+}
+
+
+sub mulops {
+   ###########################################################################################
+   # If no mulops_command argument passed - then just list all avaliable commands/descriptions
+   # Else - process the command + arguments
+
+   my ($mulops_command_arg,$mulops_mode,$catalog_cache,$mulops_output_cache) = @_;
+
+   return if defined($mulops_output_cache->{$mulops_command_arg}->{exit_code}) ;
+
+   my @catalogs_list = keys %$catalog_cache ;
+   foreach my $catalog (@catalogs_list) {
+         my $mulops_catalog;
+         if (! defined($catalog_cache->{$catalog}->{'catalog'})) {
+            # print "Cache Add : $catalog \n";
+            $mulops_catalog = &getCatalog($catalog) ;
+            $catalog_cache->{$catalog} = $mulops_catalog;
+         } else {
+            # print "Cache Find : $catalog \n";
+             $mulops_catalog = $catalog_cache->{$catalog} ;
+         }
+         my $cat_description = $mulops_catalog->{'catalog-description'} ;
+         my $cat_name = $mulops_catalog->{'catalog'} ;
+         my $cat_version = $mulops_catalog->{'catalog-version'} ;
+         my $cat_author = $mulops_catalog->{'author'} ;
+         my @cat_commands = keys %{$mulops_catalog->{'mulops-commands'}} ;
+
+         if ( $mulops_command_arg eq 'all' ) {
+            foreach my $cat_command (@cat_commands) {
+               mulops($cat_command,$mulops_mode,$catalog_cache,$mulops_output_cache) ;
+            }
+           next;
+         }
+
+         my $mulops_oscommand_variations = $mulops_catalog->{'mulops-commands'}->{$mulops_command_arg}->{'variations'} ; # Array
+
+         if ($mulops_oscommand_variations) { 
+
+            my $mulops_oscommand_description = $mulops_catalog->{'mulops-commands'}->{$mulops_command_arg}->{'description'} ; # Scalar
+            $mulops_oscommand_description = 'NoValues' unless defined $mulops_oscommand_description ;
+            $mulops_output_cache->{$mulops_command_arg}->{description} = $mulops_oscommand_description ;
+
+            $mulops_output_cache->{$mulops_command_arg}->{cat_name} = $cat_name ;
+
+            my $variation_arraysize = scalar @$mulops_oscommand_variations ;
+            my $variation_lastelement =  $variation_arraysize - 1 ;
+            foreach my $oscommand_index (0 .. $variation_lastelement) {
+               my @mulops_oscommand_variation_fields = (keys %{$mulops_oscommand_variations->[$oscommand_index]});
+               # all fields except "oscommand" are used as "selectors" (ie: lookup values )
+               my @oscommand_selector_fields = grep !/oscommand/, @mulops_oscommand_variation_fields;
+               my $selectors_match = "yes" ;
+               foreach my $oscommand_selector_field (@oscommand_selector_fields) {
+                  my $cat_selector_operator = $mulops_oscommand_variations->[$oscommand_index]->{$oscommand_selector_field}->[0];
+                  my $cat_selector_val = $mulops_oscommand_variations->[$oscommand_index]->{$oscommand_selector_field}->[1];
+                  mulops($oscommand_selector_field,"execute",$catalog_cache,$mulops_output_cache) ;
+                  my $mulops_selector_value = $mulops_output_cache->{$oscommand_selector_field}->{execute};
+                  my $match_test="\"$mulops_selector_value\" $cat_selector_operator \"$cat_selector_val\"";
+                  if ( ! eval $match_test ) {
+                     $selectors_match = "no" ;
+                     last ;   # stop checking the @oscommand_selector_fields
+                  }
+               }
+               if ( "$selectors_match" eq "no" ) {
+                  # Try the next variation
+                  next ;
+               }
+
+               # So  - at this point we have found the matching record.
+               return if ( $mulops_mode eq 'description' ) ;
+
+               my $mulops_oscommand_variation = $mulops_oscommand_variations->[$oscommand_index]->{'oscommand'};
+               if ($mulops_oscommand_variation) {
+                  
+                  # Put oscommand into the cache
+                  $mulops_output_cache->{$mulops_command_arg}->{oscommand} = $mulops_oscommand_variation ;
+                  # Put oscommands catalog json into the cache
+                  my $catalog_output = to_json($mulops_oscommand_variations->[$oscommand_index], {utf8 => 1, pretty => 1}) ;
+                  $mulops_output_cache->{$mulops_command_arg}->{catalog} = $catalog_output ;
+                  return if ( $mulops_mode eq 'catalog' ) ;
+
+                  if ($mulops_mode eq "execute") {
+                     if ( not defined($mulops_output_cache->{$mulops_command_arg}->{execute})) {
+                        if ( $mulops_command_arg =~ /.doc$/ ) {
+                           my $mulops_doc = printDoc($mulops_oscommand_variation) ;
+                           $mulops_output_cache->{$mulops_command_arg}->{exit_status} = 'pass' ;
+                           $mulops_output_cache->{$mulops_command_arg}->{execute} = $mulops_doc ;
+                        } else {
+                           # Execute the OS command
+                           # print "Execute: $mulops_command_arg \n" ; # very useful debug line for checking the cacheing !
+                           my ($merged, $return) = capture_merged {
+                              return system( $mulops_oscommand_variation );
+                           };
+                           if ( $return != 0 ) {
+                              $mulops_output_cache->{$mulops_command_arg}->{exit_status} = 'fail' ;
+                           } else {
+                              $mulops_output_cache->{$mulops_command_arg}->{exit_status} = 'pass' ;
+                           }
+                           chomp($merged);
+                           $mulops_output_cache->{$mulops_command_arg}->{execute} = $merged ;
+                           # print "RJ: mulops_command_arg cached add: $merged \n";
+                        }
+                     }  
+                  }
+               } 
+               # Stop checking catalogs ( found catalog entry - and processed all variations )
+               # Yes- that means expect all variations of one mulops_command to be in the same catalog.
+               return ;
+            }
+         }
+   }
+   #print "Processed all catalogs and not found $mulops_command_arg \n";
+   return if $mulops_command_arg eq 'all' ;
+ 
+   my $NoValues = 'NoValues (Please email Richard.Jamieson@scsuk.net, if you can fill in the blanks)';
+   $mulops_output_cache->{$mulops_command_arg}->{execute} = "$NoValues" if ! defined($mulops_output_cache->{$mulops_command_arg}->{execute}) ;
+   $mulops_output_cache->{$mulops_command_arg}->{exit_status} = 'fail' if ! defined($mulops_output_cache->{$mulops_command_arg}->{exit_status});
+   $mulops_output_cache->{$mulops_command_arg}->{oscommand} = "$NoValues" if ! defined($mulops_output_cache->{$mulops_command_arg}->{oscommand}) ;
+   $mulops_output_cache->{$mulops_command_arg}->{catalog} = 'NoValues' if ! defined($mulops_output_cache->{$mulops_command_arg}->{catalog}) ;
+   $mulops_output_cache->{$mulops_command_arg}->{cat_name} = 'NoValues' if ! defined($mulops_output_cache->{$mulops_command_arg}->{cat_name}) ;
+   $mulops_output_cache->{$mulops_command_arg}->{description} = 'NoValues' if ! defined($mulops_output_cache->{$mulops_command_arg}->{description}) ;
+}
+###########################################################################################
+# Params = catalog
+# Return = hashref
+sub getCatalog {
+   # Can add another parameter later - to signify format and maybe source of catalog
+   my ($catalog) = @_;
+   #print "Get Catalog : $catalog \n";
+   my $mulops_catalog ;
+   #      if (( -r $catalog ) || ( $catalog =~ "^http:" )) {
+   if ( $catalog =~ "^http:" ) {
+      my $http = HTTP::Lite->new;
+      my $req = $http->request("$catalog") or die "Unable to get document: $!";
+      die "Request failed ($req): ".$http->status_message() if $req ne "200";
+      my $mulops_url_data = $http->body();
+      $mulops_catalog = from_json($mulops_url_data);
+   } else {
+     my $mulops_file_data;
+     local $/; #Enable 'slurp' mode
+     open my $fh, "<", "$catalog";
+     $mulops_file_data = <$fh>;
+     close $fh;
+     $mulops_catalog = from_json($mulops_file_data);
+   }
+   return $mulops_catalog ;
+}
+###########################################################################################
+# printDoc
+sub printDoc {
+   my $default_doc_dir = $ENV{MULOPS_DOCS};
+   my $mulops_doc = $default_doc_dir . '/' . $_[0];		# Name the file
+   #open(INFO, "$mulops_doc") or die "No Doc: '$mulops_doc'";		# Open the file
+   open(INFO, "$mulops_doc") or return("No doc: $mulops_doc");		# Open the file
+   my @lines = <INFO>;		# Read it into an array
+   close(INFO);			# Close the file
+   return "@lines";			# Print the array
+}
+
+1;
+__END__
+
+=head1 NAME
+
+ mulops - Multi Operating System tool
+          Simple consistent commands..
+
+=head1 SYNOPSIS
+
+ use MULOPS;	# Imports "mulops" subroutine
+
+ # Initialise mulops_output_cache (ie: dont do this again for subsequent mulops calls )
+ my $mulops_output_cache = {}   ;
+
+ # Initialise catalog_cache (ie: dont do this again for subsequent mulops calls )
+ my $env_catalogs = $ENV{'MULOPS_CATALOGS'};
+ my @catalogs = split(' ', $env_catalogs) if defined($env_catalogs);
+ if (! @catalogs) {
+   @catalogs = glob("$default_catalogs_dir/mulcat.*.json");
+ }
+ my %catalog_cache = map { $_ => {} } @catalogs;
+ my $catalog_cache = \%catalog_cache ;
+
+ # Pick a mulops-command - eg: "model'
+ $mulops_command='model';
+
+ # Run Mulops
+ mulops($mulops_command,$mulops_mode,$catalog_cache,$mulops_output_cache) ;
+
+ # After running mulops $mulops_output_cache will have been populated.
+ $mulops_output_cache->{$mulops_command}->{cat_name}   # name of catalog
+ $mulops_output_cache->{$mulops-command}->{"catalog"}      # catalog entry from catalog
+ $mulops_output_cache->{$mulops-command}->{"description"}  # description of mulops-command
+ $mulops_output_cache->{$mulops-command}->{"oscommand"}    # the OS command
+ $mulops_output_cache->{$mulops-command}->{"execute"}      # Execution output (if any)
+ $mulops_output_cache->{$mulops-command}->{"exit_status"}  # Exit sttus of oscommand (if any)
+
+ # Can now extract results from the catalog
+ eg:
+ $Server_model = $mulops_output_cache->{"model"}->{"execute"};
+ print "Server model is $Server_model";
+
+
+ ------------------------------------------------------------------------
+ @catalogs:	 	array of mulops JSON catalog names
+ $mulops_command_arg:	mulops command argument
+ $mulops_mode:		["execute"|"oscommand"|"catalog"]
+			execute:  the oscommand is executed.
+			oscommand: the oscommands is displayed only.
+			catalog:  the catalog entry for the command is displayed.
+ $catalog_cache:	Can pass cached catalogs.
+			Only expect this to be used by mulops itself for recursion routines.
+ $mulops_output_cache   Can pass cached mulops results ( eg: descriptions, oscommands and execution-output )
+			$mulops_output_cache->{$mulops-command}->{"description"}
+			$mulops_output_cache->{$mulops-command}->{"oscommand"}
+			$mulops_output_cache->{$mulops-command}->{"execute"}
+
+=head1 DESCRIPTION
+
+ Different operating systems have different ways of doing the same thing.
+ ( eg: showing total amount of system memory ).
+ This tool aims to help to provide a consistent way to do the same things
+ across multiple operating systems.
+ The tool can be used for one-off commands, to create portable system
+ programs, or just for education
+
+=head1 INSTALL
+
+ Follow instructions in "mulops readme" - or...
+ Copy this module to a directory in your $PERL5LIB search path.
+
+=head1 AUTHORS
+
+ Richard Jamieson richard.jamieson@scsuk.net
+
+
