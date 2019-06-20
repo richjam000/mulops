c6f98fe2523805a351e7f588573eadc0b4849b5b
Richard Jamieson <richard.jamieson@sscsuk.net>
2019-06-20 23:59:23 +0100

    --restart=always

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 docker volume create mulops_html
 docker volume create mulops_cgi-bin
 docker volume create mulops_logs
 docker volume create mulops_conf
-docker run -d --name=mulops --network=jamnet  --hostname=mulops \
+
+docker run -d --name=mulops --network=jamnet --hostname=mulops --restart=always \
 --volume mulops_html:/var/www/html \
 --volume mulops_cgi-bin:/var/www/cgi-bin \
 --volume mulops_logs:/var/log/httpd \
