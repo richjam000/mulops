03b0f934d9423011b56d77ff2618a2f9caf7e237
Richard Jamieson <richard.jamieson@sscsuk.net>
2019-06-20 17:35:18 +0100

    Build from scs_httpd

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,10 +1,10 @@
-docker volume create mulops_htdocs
+docker volume create mulops_html
 docker volume create mulops_cgi-bin
 docker volume create mulops_logs
 docker volume create mulops_conf
 docker run -d --name=mulops --network=jamnet  --hostname=mulops \
---volume mulops_htdocs:/usr/local/apache2/htdocs \
---volume mulops_cgi-bin:/usr/local/apache2/cgi-bin \
---volume mulops_logs:/usr/local/apache2/logs \
---volume mulops_conf:/usr/local/apache2/conf \
-localhost:5000/scsuk.net/mulops
+--volume mulops_html:/var/www/html \
+--volume mulops_cgi-bin:/var/www/cgi-bin \
+--volume mulops_logs:/var/log/httpd \
+--volume mulops_conf:/etc/httpd \
+localhost:5000/scsuk.net/mulops:1.0
