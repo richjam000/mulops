0bcf13822cad1f5b8f2b87dbb410b692226689a4
Richard Jamieson <richard.jamieson@sscsuk.net>
2019-06-20 21:55:54 +0100

    added push to build script

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,2 +1,3 @@
 # Example docker build command
 docker build -t localhost:5000/scsuk.net/mulops:1.0 -f docker/Dockerfile https://gitlab.scsuk.net/rich/mulops.git#master
+docker push localhost:5000/scsuk.net/mulops:1.0
