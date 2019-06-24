b97dc98e0984a2d964ce792b62d53b2fff65ade7
Richard Jamieson <richard.jamieson@sscsuk.net>
2019-06-24 09:36:04 +0100

    code under scsuk  now

--- docker/build_image.sh
+++ docker/build_image.sh
@@ -1,3 +1,3 @@
 # Example docker build command
-docker build -t localhost:5000/scsuk.net/mulops:1.0 -f docker/Dockerfile https://gitlab.scsuk.net/rich/mulops.git#master
+docker build -t localhost:5000/scsuk.net/mulops:1.0 -f docker/Dockerfile https://gitlab.scsuk.net/scsuk/mulops.git#master
 docker push localhost:5000/scsuk.net/mulops:1.0
